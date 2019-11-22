f9d44c9d6b36a0c547d46553787fb71cc99b6871
netohelvecio <helvecioneto77@gmail.com>
2019-11-22 20:18:56 -0300

    usando styled-components no app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
@@ -6,6 +7,11 @@ import Routes from './routes';
 
 export default class App extends Component {
   render() {
-    return <Routes />;
+    return (
+      <>
+        <StatusBar barStyle="ligh-content" backgroundColor="#7159C1" />
+        <Routes />
+      </>
+    );
   }
 }
